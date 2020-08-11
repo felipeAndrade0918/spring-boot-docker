e9a8f303f72a97d3b6c2ed75fa0c6aec95abe72a
Felipe Andrade <felipe.andrade0918@gmail.com>
2020-08-11 16:42:06 -0300

    [feat] First commit!

---
+++ src/main/java/com/felipeandrade0918/javadocker/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.felipeandrade0918.javadocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello my friend, stay a while and listen...";
+	}
+}
